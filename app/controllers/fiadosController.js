9bdcdca93f3ad2ccc64e012c7ad229eadf1fc950
Jeyson Villalobos <jeyson3213@hotmail.com>
2018-08-25 16:54:19 -0500

    Diseño de fiadosDetalles

--- app/controllers/fiadosController.js
+++ app/controllers/fiadosController.js
@@ -5,4 +5,8 @@ router.get('/',function(req,res){
 	res.render('fiados',{layout:false});
 });
 
+router.get('/:id',function(req,res){
+	res.render('fiadosDetalles',{layout:false});
+});
+
 module.exports = router;
